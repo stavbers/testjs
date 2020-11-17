fdf53c04bf7cb345ab6c010c7413f6561227a108
reddevweb <reddevweb@gmail.com>
2020-11-18 01:55:15 +0300

    new test.js

--- test.js
+++ test.js
@@ -223,11 +223,12 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?',''),
-    c = prompt('Один из последних просмотренных фильмов?',''),
-    d = prompt('На сколько оцените его?','');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?',''),
+//     c = prompt('Один из последних просмотренных фильмов?',''),
+//     d = prompt('На сколько оцените его?','');
 
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
-    console.log(personalMovieDB);
+//     personalMovieDB.movies[a] = b;
+//     personalMovieDB.movies[c] = d;
+//     console.log(personalMovieDB);
+
