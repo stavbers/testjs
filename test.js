38fd8c172833e7b7ba1bebc69310b00be749eaea
reddevweb <reddevweb@gmail.com>
2020-11-18 00:59:43 +0300

    add first hw

--- test.js
+++ test.js
@@ -211,3 +211,23 @@ console.log(count);
 // console.log(Answer(5));
 // info = Answer(userAnswerNumber);
 // console.log(info);
+
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?',''),
+    c = prompt('Один из последних просмотренных фильмов?',''),
+    d = prompt('На сколько оцените его?','');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+    console.log(personalMovieDB);
